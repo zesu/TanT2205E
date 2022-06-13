1daca5657c189e288e6c1ed51cec48e1a30210a1
zesu <tannv.online@gmail.com>
2022-06-13 18:31:46 +0700

    Create 0106.c
    Tannv nộp bài

---
+++ 0106.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    int so1 = 0;
+    int so2 = 0;
+    int sum = 0;
+    printf("nập vào số 1:");
+    scanf("%d\n", &so1);
+
+    printf("nập vào số 2:");
+    scanf("%d\n", &so2);
+    // thực tế thông tin không đúng nội dung đăng nhập
+    
+
+    sum = so1 + so2;
+    printf("Tổng của 2 số = %d\n", sum);
+)
+for (for i = 0; i < count; i++)
+{
+if (int i=0; i < count; i++);
+{// thực hiên thông tin code ở đây
+
+    /* code */
+}
+
+if (sum % 2 == 0)
+{
+    printf("%d đây là số chẵn \n", sum);
+}
+else
+{
+    printf("%d đây là số lẻ\n", sum);
+}
+return sum;
+}
